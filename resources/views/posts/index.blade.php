04f86a92547322e214e0d678cf150249558d9f1c
johnmuthua <caveomuthama@gmail.com>
2020-12-06 17:30:16 +0300

    Added liked button

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="flex justify-center">
         <div class="w-8/12 bg-white p-6 rounded-lg">
-        <form action="{{route('posts')}}" method="POST">
+        <form action="{{route('posts')}}" method="POST" class="mb-4">
             @csrf
             <div class="mb-4">
                 <label for="body" class='sr-only'>Body</label>
@@ -13,7 +13,7 @@
 
                 @error('body')
                     <div class="text-red-500 mt-2 text-sm">
-                        {{message}}
+                        {{ $message}}
                     </div>
                 @enderror
             </div>
@@ -22,6 +22,28 @@
                 font-medium">Post</button>
             </div>
         </form>
+
+        @if($posts->count())
+        @foreach ($posts as $post)
+            <div class="mb-4">
+                <a href="" class="font-bold">{{$post->user->username}}</a> <span class="text-gray-600 text-sm">{{$post->created_at->diffForHumans()}}</span>
+                <p class="mb-2">{{$post->body}}</p>
+                <div class="flex items-center">
+                    <form action="" method="POST" class="mr-1">
+                        @csrf
+                        <button type="submit" class="text-blue-500">Like</button>
+                    </form>
+                    <form action="" method="POST" class="mr-1">
+                        @csrf
+                        <button type="submit" class="text-blue-500">Unlike</button>
+                    </form>
+                </div>
+            </div>
+        @endforeach
+            {{ $posts->links()}}
+        @else
+        no posts
+        @endif
         </div>
     </div>
 @endsection
